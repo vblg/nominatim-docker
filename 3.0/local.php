23961c435d1f7356057480a8b2334855f2359bcf
Yurchenko Stanislav <yurchenko.s@etecar.ru>
2018-04-10 11:43:53 +0300

    Added Dockerfiles for different builds, fixed nominatim behind proxy

--- 3.0/local.php
+++ 3.0/local.php
@@ -1,9 +1,9 @@
 <?php
- // Paths
+  // Paths
  @define('CONST_Postgresql_Version', '9.5');
  @define('CONST_Postgis_Version', '2.2');
  // Website settings
- @define('CONST_Website_BaseURL', '/');
+ @define('CONST_Website_BaseURL', getenv('NOMINATIM_BASE_URL') ?: '/');
  @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/monaco-updates');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
