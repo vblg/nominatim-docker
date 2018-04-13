5ee29b045fba14912b6fd2446f0492fdfe197bea
Yurchenko Stanislav <yurchenko.s@etecar.ru>
2018-04-13 12:39:07 +0300

    Added table to pg warming

--- 3.0/start.sh
+++ 3.0/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 service postgresql start
 /usr/sbin/apache2ctl -D FOREGROUND
-psql -U postgres -h $(ip -4 add show eth0|grep inet|awk '{print $2}'|cut -d/ -f1) -d nominatim -f /app/warmup.sql
+psql -U postgres -h 127.0.0.1 -d nominatim -f /app/warmup.sql
--- 3.0/warmup.sql
+++ 3.0/warmup.sql
@@ -1,3 +1,4 @@
 CREATE EXTENSION pg_prewarm;
 SELECT pg_prewarm('placex');
 SELECT pg_prewarm('search_name');
+SELECT pg_prewarm('place_addressline');
